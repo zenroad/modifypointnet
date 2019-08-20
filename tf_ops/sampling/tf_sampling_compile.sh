69ae7b64baf94e6ed1f8c9383e7baeb5bf8a434d
zhongchen <1518595517@qq.com>
2019-08-20 16:38:31 +0800

    1

--- tf_ops/sampling/tf_sampling_compile.sh
+++ tf_ops/sampling/tf_sampling_compile.sh
@@ -24,4 +24,4 @@ echo $TF_LIB
 $CUDA_ROOT/bin/nvcc tf_sampling_g.cu -o tf_sampling_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
 
 # TF>=1.4.0
-g++ -std=c++11 tf_sampling.cpp tf_sampling_g.cu.o -o tf_sampling_so.so -shared -fPIC -I$TF_INC/ -I$TF_INC/external/nsync/public -L$TF_LIB  -I$CUDA_ROOT/include -lcudart -L$CUDA_ROOT/lib64/ -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+g++ -std=c++11 tf_sampling.cpp tf_sampling_g.cu.o -o tf_sampling_so.so -shared -fPIC -I$TF_INC/ -I$TF_INC/external/nsync/public -L$TF_LIB  -I$CUDA_ROOT/include -lcudart -L$CUDA_ROOT/lib64/ -O2 
